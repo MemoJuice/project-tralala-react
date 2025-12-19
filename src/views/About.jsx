1973f9a49fedb4ba8e5151147bdfc8dcbd61b8b5
sorbemelon <67460064+Sorbemelon@users.noreply.github.com>
2025-12-19 22:09:31 +0700

    convert a tag to Link tag about, checkout, orderconfirmation

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 export default function About() {
   return (
     <div>
@@ -16,9 +18,9 @@ export default function About() {
             </p>
           </div>
           <div>
-            <a href="../index.html">
+            <Link to="/">
               <button class="block mx-auto mb-4 rounded-3xl bg-violet-200 px-3 py-2 text-xl font-semibold text-gray-800 outline-1 outline-violet-400 hover:cursor-pointer hover:bg-violet-500 hover:text-gray-100">กลับสู่หน้าหลัก</button>
-            </a>
+            </Link>
           </div>
         </div>
       </section>
