b374469c8f7a5ad604db4b9fa57e33db64040064
meanwrs <meen.warissara@hotmail.com>
2026-01-14 11:59:10 +0700

    change ui dashboard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -5,40 +5,39 @@ export default function UserCard (){
         <div className="text-center text-gray-500 mb-8">August 12, 2024
         <div className="box-content w-[400px] h-[300px] bg-white rounded-lg shadow-xl/20 mt-8 ml-6">
 
-        <div className="flex items-start p-4">
+        <div className="flex items-start p-6">
   <img
     src="images/woman.png"
     alt="user-profile"
-    className="w-30 h-30 object-cover"
+    className="w-32 rounded-full object-cover"
   />
-  <div className="ml-4 flex-1">
+  <div className="ml-6 flex-1">
     <div className="mb-2">
-      <span className="font-bold">ชื่อ-สกุล:</span>
-      <span className="ml-2">สมศรี</span>
+      <span className="inline-block w-36 font-medium">ชื่อ-สกุล:</span>
+      <span>สมศรี</span>
     </div>
-    <div className="text-sm text-gray-500 mb-1">
-      <span className="inline-block w-32">วันเกิด:</span>
+    <div className="mb-2">
+      <span className="inline-block w-36 font-medium">วันเกิด:</span>
       <span>12 ธันวาคม 1954</span>
     </div>
-    <div className="text-sm text-gray-500 mb-1">
-      <span className="inline-block w-32">อายุ:</span>
+    <div className="mb-2">
+      <span className="inline-block w-36 font-medium">อายุ:</span>
       <span>71 ปี</span>
     </div>
-    <div className="text-sm text-gray-500 mb-1">
-      <span className="inline-block w-32">เพศ:</span>
+    <div className="mb-2">
+      <span className="inline-block w-36 font-medium">เพศ :</span>
       <span>หญิง</span>
     </div>
-    <div className="text-sm text-gray-500 mb-1">
-      <span className="inline-block w-32">ลักษณะอาการ:</span>
+    <div className="mb-2">
+      <span className="inline-block w-36 font-medium">ลักษณะอาการ :</span>
       <span></span>
     </div>
-    <div className="text-sm text-gray-500">
-      <span className="inline-block w-32">เบอร์โทรศัพท์:</span>
+    <div className="mb-2">
+      <span className="inline-block w-36 font-medium">เบอร์โทรศัพท์ :</span>
       <span></span>
     </div>
   </div>
 </div>
-
         {/* <div className="bg-cyan-100 text-gray-800 px-6 py-3 rounded-lg flex gap-4">
         <span className="font-medium">status:Active</span>
             <div className="flex gap-4">
@@ -51,9 +50,11 @@ export default function UserCard (){
             </div> */}
           </div>
         </div>
-        </div>
-        </div>
-        </div>
+
+        // </div>
+        // </div>
+        // </div>
+       
       
 
     )
