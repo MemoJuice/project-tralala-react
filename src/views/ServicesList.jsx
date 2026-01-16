e33c88a253b51d78f2d4805a1cadc7a399de0abc
phacharaM <phachara.mingkhwan@gmail.com>
2026-01-16 13:45:19 +0700

    pop-up card in SL

--- src/views/ServicesList.jsx
+++ src/views/ServicesList.jsx
@@ -1,5 +1,8 @@
-import DailyPlanCard from "@/components/serviceslist/DailyPlanCard";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+
+import DailyPlanCard from "@/components/serviceslist/DailyPlanCard";
 import MonthlyplanCard from "@/components/serviceslist/MonthlyplanCard";
 import HospitalPlanCard from "@/components/serviceslist/HospitalPlanCard";
 import MenuBar from "@/components/serviceslist/MenuBar";
@@ -12,11 +15,46 @@ export default function ServicesList (){
     return (
         <div className="flex flex-col justify-evenly px-10 pb-10 items-center">
           <MenuBar setView={setView} />
+          
           <main className="items-center w-full justify-center">
-              {view === "hospitalplan" && <HospitalPlanCard />}
-              {view === "dailyplan" && <DailyPlanCard />}
-              {view === "monthlyplan" && <MonthlyplanCard />}
-          </main>
+            <AnimatePresence mode="wait">
+          {view === "hospitalplan" && (
+            <motion.div
+              key="hospital"
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -40 }}
+              transition={{ duration: 0.4 }}
+            >
+              <HospitalPlanCard />
+            </motion.div>
+          )}
+
+          {view === "dailyplan" && (
+            <motion.div
+              key="daily"
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -40 }}
+              transition={{ duration: 0.4 }}
+            >
+              <DailyPlanCard />
+            </motion.div>
+          )}
+
+          {view === "monthlyplan" && (
+            <motion.div
+              key="monthly"
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -40 }}
+              transition={{ duration: 0.4 }}
+            >
+              <MonthlyplanCard />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </main>
         </div>
   );
 }
