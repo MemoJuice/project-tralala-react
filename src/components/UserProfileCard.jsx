10c3b95eb407da9fac32dcb024bbac9715045543
meanwrs <meen.warissara@hotmail.com>
2026-01-19 09:45:20 +0700

    refactor userdashboardand added schudule component

--- src/components/UserProfileCard.jsx
+++ src/components/UserProfileCard.jsx
@@ -1,7 +1,7 @@
 export default function UserProfileCard() {
   return (
     // <div className="text-center text-gray-500 mb-8">
-    <div className="w-800 h-90 bg-white rounded-lg shadow-xl/20  mt-10 ml-30">
+    <div className="w-200 h-90 bg-white rounded-lg shadow-xl/20  mt-10 ml-20">
       <div className="flex items-start p-6">
         <img src="images/woman.png" alt="user-profile" className="w-32 " />
         <div className="flex-col flex-start justify-center ml-6 flex-1  ">
